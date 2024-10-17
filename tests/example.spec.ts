6b09bddb6e53128b868ffdeb69059cae66de3c01
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2024-10-17 10:46:55 -0400

    Add percy tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,27 +1,34 @@
 import { test, expect } from '@playwright/test';
+import percySnapshot from '@percy/playwright';
 
 test('renders a primary button', async ({ page }) => {
   await page.goto('/iframe.html?args=&id=button--primary&viewMode=story');
 
+  expect(page).toBeDefined();
+
   await page.waitForSelector('ds-button');
 
-  // await expect(page).toHaveScreenshot(['button.test.png']);
+  await percySnapshot(page, 'primary-button');
 });
 
 test('renders a secondary button', async ({ page }) => {
   await page.goto('/iframe.html?args=&id=button--secondary&viewMode=story');
 
+  expect(page).toBeDefined();
+
   await page.waitForSelector('ds-button');
 
-  // await expect(page).toHaveScreenshot(['secondary.test.png']);
+  await percySnapshot(page, 'secondary-button');
 });
 
 test('renders a primary small button', async ({ page }) => {
   await page.goto('/iframe.html?args=&id=button--primary-small&viewMode=story');
 
+  expect(page).toBeDefined();
+
   await page.waitForSelector('ds-button');
 
-  // await expect(page).toHaveScreenshot(['primary-small.test.png']);
+  await percySnapshot(page, 'primary-small-button');
 });
 
 test('renders a secondary small button', async ({ page }) => {
@@ -29,7 +36,9 @@ test('renders a secondary small button', async ({ page }) => {
     '/iframe.html?args=&id=button--secondary-small&viewMode=story',
   );
 
+  expect(page).toBeDefined();
+
   await page.waitForSelector('ds-button');
 
-  // await expect(page).toHaveScreenshot(['secondary-small.test.png']);
+  await percySnapshot(page, 'secondary-small-button');
 });
